163daa11c7d1a73e59ce89584bb80917078dde74
harbouli <mharbouli@icloud.com>
2021-10-28 16:28:09 +0100

    Starting Forms

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Register = () => {
     return (
         <div>
-            
+            <form className="form">
+                <labe>
+                    Firstname:
+                </labe>
+                <input type="text" name="Firstname" className="form-input"/>
+            </form>
         </div>
     )
 }
